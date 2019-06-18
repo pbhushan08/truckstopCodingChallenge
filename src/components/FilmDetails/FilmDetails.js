24bd962880f4622310fd4921b4a519a8629c9992
prachi <pbhushan@hawk.iit.edu>
2019-06-18 07:45:55 -0500

    film details displayed

--- src/components/FilmDetails/FilmDetails.js
+++ src/components/FilmDetails/FilmDetails.js
@@ -3,22 +3,27 @@ import './FilmDetails.css';
 //import logo from '../../assets/logo.png';
 
 //   
-     
+//const date = new Date((release_date + " 00:00:00")).toDateString()
 
 const filmDetails = ({films}) => (
-   
-   <ul>
-        {films.map(({ title, release_date,director, producer }) => {
-          const date = new Date((release_date + " 00:00:00")).toDateString()
-          return (
-            <li key={title}>
-              {title} ({date})
-            </li>
-          );
-        
-       })}
-      </ul>
-) 
+    <div className = "list">
+    <div className = "button">X</div>
+    <h2>FILM DETAILS</h2>
+    <ul>
+    {films.map(({ title, release_date, director }) => {
+        const date = new Date((release_date + " 00:00:00")).toDateString()
 
+      return (
+        <li key={title} className ="films__header">
+                {`Title: ${title}`} ({date})
+                     {`Director: ${director}`}
+        </li>
+      );
+    })}
+  </ul>
+</div>
+
+)
+    
 
 export default filmDetails;
